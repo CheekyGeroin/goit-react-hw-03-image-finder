50041be3885b4a8d9b81064c196acbb06d9adc81
Danylo Altukhov <dan.97198@gmail.com>
2022-09-22 20:31:06 +0300

    make map of images

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -11,21 +11,38 @@ class ImageGallery extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     const { item, page } = this.props;
+    const { images } = this.state;
+    const prevImages = prevState.images;
+    const nextImages = this.state.images;
     const prevItem = prevProps.item;
     const nextItem = item;
 
     if (prevItem !== nextItem) {
       api
         .fetchImages(nextItem, page)
-        // .then(console.log)
-        // .catch(error => this.setState({ error }));
+        .then(images => {
+          images.hits.map(({ id, webformatURL, largeImageURL, tags }) => {
+            const image = {
+            id,
+            tags,
+            smallImage: webformatURL,
+            largeImage: largeImageURL,
+          }
+          this.setState(({ images }) => ({
+            images: [image, ...images]
+          }))
+          })
+        })
+        .catch(error => this.setState({ error }));
     }
+    
   }
 
   render() {
     const { images } = this.state;
+    console.log(images)
       return (
-          <ImageGallery images={images} />
+          <ImageGalleryItem images={images} />
     )
   }
 }
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 
 export const ImageGalleryItem = ({ images }) => {
   return (
-      images.map(({ id, webformatURL, largeImageURL }) => {
+      images.map(({ id, webformatURL, largeImageURL, tags }) => {
           <li key={id}>
-            <img src={webformatURL} alt={id} />
+            <img src={webformatURL} alt={tags} />
         </li>
     })
   );
